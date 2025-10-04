509b01c8e356e8a15f9754352d6725d3fbd22263
MaryemHadjWannes <bentwannes@gmail.com>
2025-10-04 21:06:30 +0100

    specify butcher client

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+  },
+  preview: {
+    allowedHosts: ["boucherie-halal.onrender.com"], // 👈 add this line
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
